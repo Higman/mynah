ffaf2182fd3e9bf504d7b42e1895e0a8f4bb8f69
Higman <s19g451@stu.kagawa-u.ac.jp>
2020-07-09 21:56:20 +0900

    init commit

---
+++ src/main/java/jp/ac/kagawalab/mynah/MynahApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.kagawalab.mynah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynahApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MynahApplication.class, args);
+    }
+
+}
